0a8b24b37504daa65a100b1c950d07388aab25fb
Craig Hillelson <craig.hillelson@intersection.com>
2018-10-16 00:09:14 -0400

    lets user know their percentage correct

--- v1/terms_and_definitions.py
+++ v1/terms_and_definitions.py
@@ -1,8 +1,9 @@
-# import
+# imports
 import csv
+import random
 
+# define lambda
 rtn = lambda : '\n'
-print(rtn())
 
 # create lists to be populated later
 terms = []
@@ -11,17 +12,20 @@ corrects = []
 incorrects = []
 
 # import a csv with terms and definitions and populate lists of terms and definitions
-with open('terms.csv') as f:
+with open('terms_and_definitions.csv') as f:
 	f_csv = csv.DictReader(f)
 	for row in f_csv:
 		terms.append(row['term'])
 		definitions.append(row['definition'])
 
-# set i to 0
-i = 0
+number_to_drill = int(raw_input("How many terms would you like to drill? "))
+random_numbers = random.sample(range(0, len(terms)), number_to_drill)
+
+# for readability
+print(rtn())
 
 # loop through terms, quiz user, and add correct and incorrect answers to respective lists
-for term in terms:
+for i in random_numbers:
 	print(rtn())
 	prompt = "Term: %s\nDefinition: " % (terms[i])
 	user_answer = raw_input(prompt) 
@@ -32,15 +36,19 @@ for term in terms:
 		print("Incorrect")
 		print("%s") % (definitions[i])
 		incorrects.append(terms[i])
-	i = i + 1
 
+# update user on their performance
 # get length of each set of answers
 num_correct = len(corrects)
 num_inconnect = len(incorrects)
 
 print(rtn())
 
-# update user on their performance
+percentage_correct = float(len(corrects)) / float(len(random_numbers))
+print("You defined %s of the terms you attempted correctly.") % ("{0:.0%}".format(percentage_correct))
+
+print(rtn())
+
 print("%s %s") % ("Correct Answers:", num_correct)
 for correct in corrects:
 	print(correct)
@@ -48,6 +56,7 @@ for correct in corrects:
 print(rtn())
 
 print("%s %s") % ("Incorrect Answers:", num_inconnect)
+print("Review the following:")
 for incorrect in incorrects:
 	print(incorrect)
 
