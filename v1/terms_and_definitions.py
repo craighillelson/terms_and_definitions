abcbc0a12cd50aec6d97d586d35e2089d53870d0
Craig Hillelson <craig.hillelson@intersection.com>
2019-01-05 11:58:12 -0500

    refactored based on pylint findings

--- v1/terms_and_definitions.py
+++ v1/terms_and_definitions.py
@@ -1,63 +1,66 @@
+""" __doc__ """
+
 # imports
 import csv
 import random
 
 # define lambda
-rtn = lambda: '\n'
+RTN = lambda: '\n'
 
 # create lists to be populated later
-terms = []
-definitions = []
-corrects = []
-incorrects = []
+TERMS = []
+DEFINITIONS = []
+CORRECTS = []
+INCORRECTS = []
 
 # import a csv with terms and definitions and populate lists of terms and definitions
 with open('terms_and_definitions.csv') as f:
-    f_csv = csv.DictReader(f)
-    for row in f_csv:
-        terms.append(row['term'])
-        definitions.append(row['definition'])
+    F_CSV = csv.DictReader(f)
+    for row in F_CSV:
+        TERMS.append(row['term'])
+        DEFINITIONS.append(row['definition'])
 
-number_to_drill = int(raw_input("How many terms would you like to drill? "))
-random_numbers = random.sample(range(0, len(terms)), number_to_drill)
+NUMBER_TO_DRILL = int(raw_input("How many terms would you like to drill? "))
+RANDOM_NUMBERS = random.sample(range(0, len(TERMS)), NUMBER_TO_DRILL)
 
 # for readability
-print(rtn())
+print RTN()
 
 # loop through terms, quiz user, and add correct and incorrect answers to respective lists
-for i in random_numbers:
-    print(rtn())
-    prompt = "Term: %s\nDefinition: " % (terms[i])
-    user_answer = raw_input(prompt) 
-    if user_answer == definitions[i]:
-        print("Correct!")
-        corrects.append(terms[i])
+for i in RANDOM_NUMBERS:
+    print RTN()
+    prompt = "Term: %s\nDefinition: " % (TERMS[i])
+    user_answer = raw_input(prompt)
+    if user_answer == DEFINITIONS[i]:
+        print "Correct!"
+        CORRECTS.append(TERMS[i])
     else:
-        print("Incorrect")
-        print("%s") % (definitions[i])
-        incorrects.append(terms[i])
+        print "Incorrect"
+        print "%s"  % (DEFINITIONS[i])
+        INCORRECTS.append(TERMS[i])
 
 # update user on their performance
 # get length of each set of answers
-num_correct = len(corrects)
-num_inconnect = len(incorrects)
+NUM_CORRECT = len(CORRECTS)
+NUM_INCONNECT = len(INCORRECTS)
 
-print(rtn())
+print RTN()
 
-percentage_correct = float(len(corrects)) / float(len(random_numbers))
-print("You defined %s of the terms you attempted correctly.") % ("{0:.0%}".format(percentage_correct))
+PERCENTAGE_CORRECT = float(len(CORRECTS)) / float(len(RANDOM_NUMBERS))
+print "You defined %s of the terms you attempted correctly." % ("{0:.0%}".format
+                                                                (PERCENTAGE_CORRECT))
 
-print(rtn())
+print RTN()
 
-print("%s %s") % ("Correct Answers:", num_correct)
-for correct in corrects:
-    print(correct)
+print "%s %s" % ("Correct Answers:", NUM_CORRECT)
+for correct in CORRECTS:
+    print correct
 
-print(rtn())
+print RTN()
 
-print("%s %s") % ("Incorrect Answers:", num_inconnect)
-print("Review the following:")
-for incorrect in incorrects:
-    print(incorrect)
+print "%s %s" % ("Incorrect Answers:", NUM_INCONNECT)
+print "Review the following:"
+for incorrect in INCORRECTS:
+    print incorrect
 
-print(rtn())
+print RTN()
